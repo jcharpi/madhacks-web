7daecb6de62e1856e9248cf0674c01b6601ccfdd
Josh Charpentier <charpentier.josh@gmail.com>
2023-03-04 17:59:27 -0600

    Cards work!

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { Card, Col } from "react-bootstrap";
+import { Button, Card, Col } from "react-bootstrap";
 
 interface Props {
     exercise: string;
 }
 
 const ExerciseCard = (props: Props) => {
+
+    function handleClick() {
+        console.log("hi")
+    }
+
     return <Col className="select--col" xs={12} sm={12} md={6} lg={4} xl={4}>
-        <Card className="select--card">
+        <Card onClick={handleClick} className="select--card stretched-link">
             <Card.Title className="select--card--title">{props.exercise}</Card.Title>
             <Card.Img variant="bottom" className="select--image" alt="Exercise" src="https://www.nhm.ac.uk/content/dam/nhmwww/discover/frog-eyes-evolution/frog-eyes-chubby-frog-flower-full-width.jpg"/>
         </Card>
